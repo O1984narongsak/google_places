7d7b772d578b897f620053a3b1865bd46c427595
Narongsak Launwijit <censpi@gmail.com>
2018-06-15 16:37:47 +0700

    first commit

---
+++ GGM/CustomMarkerView.swift
@@ -0,0 +1,9 @@
+//
+//  CustomMarkerView.swift
+//  GGM
+//
+//  Created by Narongsak Luanwijit on 11/6/2561 BE.
+//  Copyright © 2561 Narongsak Luanwijit. All rights reserved.
+//
+
+import Foundation
---
+++ GGM/RestaurantPreviewView.swift
@@ -0,0 +1,9 @@
+//
+//  RestaurantPreviewView.swift
+//  GGM
+//
+//  Created by Narongsak Luanwijit on 11/6/2561 BE.
+//  Copyright © 2561 Narongsak Luanwijit. All rights reserved.
+//
+
+import Foundation
